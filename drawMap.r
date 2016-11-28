35458e24f40707b522a5dedf138a68728cc720e6
lleontan <lleontan@gmail.com>
2016-11-28 11:51:53 -0800

    added mapdrawing file

---
+++ drawMap.r
@@ -0,0 +1,9 @@
+# Returns a plotly or ggplot2 map.
+# Takes a country and year as input.
+# Returns a map with an arrow pointing towards the united states size or color scaled for magnitude.
+# Possible extras when feature complete could include, a indication of emmigration vs immigration(arrow direction),
+#   Region indication, arrow style, uptrend or downtrending level.
+
+getMap<- function(year,country){
+  
+}

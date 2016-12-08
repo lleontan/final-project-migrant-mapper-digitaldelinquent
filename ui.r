de295240d7505d51e5c1290acee5d15ba519f818
lleontan <lleontan@gmail.com>
2016-12-07 22:48:44 -0800

    added writing

--- ui.r
+++ ui.r
@@ -67,6 +67,11 @@ shinyUI(
                mainPanel(
                  plotlyOutput("total.pie.chart"),
                  br(),
+                 p("While Mexico is by far the largest contributor to immigrants to the US with more than the 3 next
+                   largest contributors it still accounts for a miniscule portion of the total. Only making up
+                  20% of all immigrants since 1980, Mexico has seen a disproportionately large response compared
+                  to its actual impact. Suprisingly The Philippines is the second largest contributor
+                  of immigrants outcompeting more far more populous China, and India."),
                  p("This pie chart displays the portion of immigration for each country.  Using the Countries tool,
                    you can move the slider to decide the number of countries to display the immigration statistics. 
                    Leaving the slider at one will display Mexico's immigration statistics and compare it to all the other
@@ -88,7 +93,20 @@ shinyUI(
                    h3("Immigrant of specific country"),
                    p(textOutput("countryInfo")),
                    br(),
-                   plotlyOutput("searchCountry"),
+                   plotlyOutput("searchCountry")
+                   ,span("Interesting events:"),
+                   br(),
+                   span("Peak of FARC power in Columbia in the 2000's."),
+                   br(),
+                    span("Collapse of the Soviet Union in 1991:Estonia, Unknown, Poland, Latvia"),
+                   br(),
+                   span("Tienanmen Square 1989:Hongkong"),
+                   br(),
+                   span("Immigration Act of 1990:Total, El Salvador, Mexico, Guatamala, Unknown"),
+                   br(),
+                   span("1997-1999 Immigration Dip:England, Canada, Australia, France, Germany"),
+                   br(),
+                   span("US withdrawl from Iraq and Arab Spring 2011:Iraq, Syria"),
                    hr()
                  )
                )
@@ -117,6 +135,13 @@ shinyUI(
                            When hovering over a bar, the statistics for that region will be displayed for that year.
                            Most regions have a steady flow of immigration, but Central America has major changes from year to year."),
                          br(),
+                         p("In the late 80's and early 90's changes to US immigration policy through the American Homecoming Act
+                           and Immigration Act of 1990 saw a surge in immigrants from Vietnam and Central America.
+                           The Immigration Act of 1990 expanded immigration quotas, increased visa accessability, removed restrictions
+                            on homosexual immigrants, created family reunifaction visas, streamlined refugee visas,
+                           and created the diversity visas program. This led to a tremendous surge in immigrants from 
+                           central america utilizing the new family visas. El Salvador produced an especially large
+                           number of migrants fleeing natural disasters using the expanded access to family reunifcation visas."),
                          br(),
                          br()
                )
@@ -132,6 +157,11 @@ shinyUI(
                p("The data throughout this website comes from", tags$a(href="http://www.un.org/en/development/desa/population/migration/data/empirical2/migrationflows.shtml", "UN Immigration Data"),
                  ". Migration statistics are important for the design of sensible migration policies, as immigrants can exert huge influence on social stability and economic development of United State.
                  By comparing the amount of immigrants flowing into US and economic gain of USA, it sheds light on the issue that whether migration has positive or negative impact to this country."),
+               p("http://ir.lawnet.fordham.edu/cgi/viewcontent.cgi?article=1270&context=ilj
+                https://www.wcl.american.edu/hrbrief/v6i3/immigration.htm
+                  http://immigrationtounitedstates.org/337-amerasian-homecoming-act-of-1987.html
+                 http://www.fairus.org/facts/us_laws
+                 http://www.migrationpolicy.org/article/salvadoran-immigrants-united-states"),
                hr(),
                h3("Developers"),
                p("Xiuxing Lao"),
